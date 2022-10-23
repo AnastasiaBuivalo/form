06e86916ea6edc9c5301e090430e93df9bbc7bab
AnastasiaBuivalo <abuyvalo@mail.ru>
2022-10-23 11:39:39 +0300

    localStorage

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 'use strict';
 
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
+
+
+if(localStorage.getItem('isChecked')){
+    checkbox.checked = true;
+};
+
+checkbox.addEventListener('change', ()=>{
+    if(localStorage.getItem('isChecked'))
+        localStorage.removeItem('isChecked');
+    else
+        localStorage.setItem('isChecked', true);
+});
+
+if(localStorage.getItem('bg')){
+    form.style.backgroundColor = 'green';
+}
+
+change.addEventListener('click', ()=>{
+    if(localStorage.getItem('bg') === 'changed'){
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = 'white';
+    }
+    else{
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'green';
+    }
+})
